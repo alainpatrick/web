353e0ea8f5325a16bd7574dd7f38465f857ae5a0
Al <alpat@Babav0ss.local>
2022-03-04 16:05:26 +0100

    fixed partners logos

--- src/lib/components/Partners.svelte
+++ src/lib/components/Partners.svelte
@@ -7,40 +7,40 @@
         Our Partners
       </h2>
     <div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 xl:grid-cols-6 gap-6">
-        <div class="h-20 px-10 py-10 bg-gray-200 flex justify-center items-center rounded-lg">
+        <div class="h-20 px-5 py-5 bg-gray-200 flex justify-center items-center rounded-lg">
             <img class="object-center h-10" src="/partners/contango.png" alt="contango">
         </div>
-        <div class="h-20 px-10 py-10 bg-gray-200 flex justify-center items-center rounded-lg">
+        <div class="h-20 px-5 py-5 bg-gray-200 flex justify-center items-center rounded-lg">
             <img class="object-center h-10" src="/partners/crust.png" alt="crust">
         </div>
-        <div class="h-20 px-10 py-10 bg-gray-200 flex justify-center items-center rounded-lg">
-            <img class="object-center h-10 rounded" src="/partners/dark/cudos.png" alt="cudos">
+        <div class="h-20 px-5 py-5 bg-gray-200 flex justify-center items-center rounded-lg">
+            <img class="object-center h-10 rounded" src="/partners/white/cudos.png" alt="cudos">
         </div>
-        <div class="h-20 px-10 py-10 bg-gray-200 flex justify-center items-center rounded-lg">
-            <img class="object-center h-10 rounded" src="/partners/dfinity.png" alt="dfinity">
+        <div class="h-20 px-5 py-5 bg-gray-200 flex justify-center items-center rounded-lg">
+            <img class="object-center h-10 rounded" src="/partners/white/dfinity.png" alt="dfinity">
         </div>
-        <div class="h-20 px-10 py-10 bg-gray-200 flex justify-center items-center rounded-lg">
+        <div class="h-20 px-5 py-5 bg-gray-200 flex justify-center items-center rounded-lg">
             <img class="object-center h-10" src="/partners/filecoin.png" alt="filecoin">
         </div> 
-        <div class="h-20 px-10 py-10 bg-gray-200 flex justify-center items-center rounded-lg">
+        <div class="h-20 px-5 py-5 bg-gray-200 flex justify-center items-center rounded-lg">
             <img class="object-center h-10" src="/partners/dark/delta-fund.png" alt="delta-fund">
         </div> 
-        <div class="h-20 px-10 py-10 bg-gray-200 flex justify-center items-center rounded-lg">
+        <div class="h-20 px-5 py-5 bg-gray-200 flex justify-center items-center rounded-lg">
             <img class="object-center h-10" src="/partners/mkd.png" alt="mkd">
         </div> 
-        <div class="h-20 px-10 py-10 bg-gray-200 flex justify-center items-center rounded-lg">
-            <img class="object-center h-10 rounded" src="/partners/dora-hacks.png" alt="dora-hacks">
+        <div class="h-20 px-5 py-5 bg-gray-200 flex justify-center items-center rounded-lg">
+            <img class="object-center h-10 rounded" src="/partners/dark/dora-hacks.png" alt="dora-hacks">
         </div> 
-        <div class="h-20 px-10 py-10 bg-gray-200 flex justify-center items-center rounded-lg">
-            <img class="object-center h-10" src="/partners/master-ventures.png" alt="max-stealth">
+        <div class="h-20 px-5 py-5 bg-gray-200 flex justify-center items-center rounded-lg">
+            <img class="object-center h-10" src="/partners/dark/master-ventures.png" alt="max-stealth">
         </div> 
-        <div class="h-20 px-10 py-10 bg-gray-200 flex justify-center items-center rounded-lg">
-            <img class="object-center h-10" src="/partners/meta-edge-ventures.png" alt="meta-edge-ventures">
+        <div class="h-20 px-5 py-5 bg-gray-200 flex justify-center items-center rounded-lg">
+            <img class="object-center h-10" src="/partners/white/meta-edge-ventures.png" alt="meta-edge-ventures">
         </div> 
-        <div class="h-20 px-10 py-10 bg-gray-200 flex justify-center items-center rounded-lg">
+        <div class="h-20 px-5 py-5 bg-gray-200 flex justify-center items-center rounded-lg">
             <img class="object-center h-10" src="/partners/protocol-labs.png" alt="protocol-labs">
         </div> 
-        <div class="h-20 px-10 py-10 bg-gray-200 flex justify-center items-center rounded-lg">
+        <div class="h-20 px-5 py-5 bg-gray-200 flex justify-center items-center rounded-lg">
             <img class="object-center h-10" src="/partners/tenzor-capital.png" alt="tenzor-capital">
         </div>    
 
