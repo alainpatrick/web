e5dfdd5450511516720986478a395f0ff66e1661
Al <alpat@Babav0ss.local>
2022-03-04 15:42:58 +0100

    removed dots

--- src/lib/components/Features.svelte
+++ src/lib/components/Features.svelte
@@ -8,9 +8,6 @@
                 <div class="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center">
                   <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
                     <div class="px-4 py-5 flex-auto">
-                      <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-red-400">
-                        <i class="fas fa-award"></i>
-                      </div>
                       <h6 class="text-2xl font-semibold">Own Your Data</h6>
                       <p class="text-xl mt-2 mb-4 text-blueGray-500">
                         Box is built from the ground up. 
@@ -26,9 +23,6 @@
                 <div class="w-full md:w-4/12 px-4 text-center">
                   <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
                     <div class="px-4 py-5 flex-auto">
-                      <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-lightBlue-400">
-                        <i class="fas fa-retweet"></i>
-                      </div>
                       <h6 class="text-2xl font-semibold">Take Back Control</h6>
                       <p class="text-xl mt-2 mb-4 text-blueGray-500">
                         Apps without ads or subscription.<br/>
@@ -42,9 +36,6 @@
                 <div class="pt-6 w-full md:w-4/12 px-4 text-center">
                   <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
                     <div class="px-4 py-5 flex-auto">
-                      <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-emerald-400">
-                        <i class="fas fa-fingerprint"></i>
-                      </div>
                       <h6 class="text-2xl font-semibold">Earn Crypto</h6>
                       <p class="text-xl mt-2 mb-4 text-blueGray-500">
                         Box quietly earns tokens by providing backups and resources to others on the network. 
